9d5dab2f764f31b096f587d9b87c7952ace2f431
cielo-ee <cielo.ee+github@gmail.com>
2015-07-08 23:50:59 +0900

    データ型を指定するようにした

--- suica.pl
+++ suica.pl
@@ -14,10 +14,30 @@ my $filename = shift;
 
 my @fields = qw/id date agency s_station s_comp s_line e_station e_comp e_line fare balance /;
 
+my $data_type = {'id'        => 'int',
+                 'date'      => 'text',
+                 'agency'    => 'text',
+                 's_station' => 'text',
+                 's_comp'    => 'text',
+                 's_line'    => 'text',
+                 'e_station' => 'text',
+                 'e_comp'    => 'text',
+                 'e_line'    => 'text',
+                 'fare'      => 'text',
+                 'balance'   => 'text'
+                 };
+
+
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=test.db");
 
 my $fieldlist = join ',',@fields;
+
+my $typelist;
+foreach (@fields){
+    $typelist .= $_." ".$data_type->{$_}.",";
+}
+
 $dbh->do("create table if not exists suica ($fieldlist)");
 
 open my $fh,'<',$filename or die "$!";
